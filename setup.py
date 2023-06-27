5115d54432186f0dcc21c838b267ecc886d96129
Ulysse Buonomo <buonomo.ulysse@gmail.com>
2023-06-26 21:28:57 -0500

    refactor: rename C file
    Better having the C file corresponding
    to the extension name.

--- setup.py
+++ setup.py
@@ -4,4 +4,4 @@ Setup for the C extension binding.
 
 from setuptools import setup, Extension
 
-setup(ext_modules=[Extension("fast_polyline_ext", sources=["src/polyline.c"])])
+setup(ext_modules=[Extension("fast_polyline_ext", sources=["src/fast_polyline_ext.c"])])
